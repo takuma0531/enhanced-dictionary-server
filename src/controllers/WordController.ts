4ce3d8076d53a571936177434e0b32d57452de4e
takuma masaki <tb.masaki0531@gmail.com>
2021-12-20 23:24:23 +0200

    add word controller

---
+++ src/controllers/WordController.ts
@@ -0,0 +1,119 @@
+import { Request, Response } from "express";
+import { IWordService } from "../services/word/IWordService";
+import { BaseController } from "./base/BaseController";
+import {
+  WordReadDto,
+  WordCreateDto,
+  WordUpdateDto,
+} from "../typings/models/word/dto";
+
+export class WordController extends BaseController {
+  constructor(private readonly _wordService: IWordService) {
+    super();
+  }
+
+  // @route     POST api/v1/words/user
+  // @desc      register unknown word for user
+  // @access    private
+  public async createWord(req: Request, res: Response) {
+    try {
+      const wordCreateDto: WordCreateDto = req.body;
+      const wordReadDto = await this._wordService.registerWord(wordCreateDto);
+      return super.created<WordReadDto>(res, wordReadDto);
+    } catch (err: any) {
+      return super.internalServerError(res, err);
+    }
+  }
+
+  // @route     GET api/v1/words/user
+  // @desc      get all words registered for user
+  // @access    private
+  public async getWordsByUserId(req: Request, res: Response) {
+    try {
+      const { id } = req.userClaims;
+      const wordReadDtos = await this._wordService.getWordsByUserId(id!);
+      if (wordReadDtos == null) return super.notFound(res);
+      return super.ok(res, wordReadDtos);
+    } catch (err: any) {
+      return super.internalServerError(res, err);
+    }
+  }
+
+  // @route     GET api/v1/words/user/memory-game?numberOfPairs=
+  // @desc      get words registered for user playing memory game
+  // @access    private
+  public async getWordsForMemoryGame(req: Request, res: Response) {
+    try {
+      const { id } = req.userClaims;
+      const { numberOfPairs } = req.params;
+      const convertedNumberOfPairs = Number(numberOfPairs);
+      const wordReadDtos = await this._wordService.getWordsForMemoryGame(
+        id!,
+        convertedNumberOfPairs
+      );
+      if (wordReadDtos == null) return super.notFound(res);
+      return super.ok(res, wordReadDtos);
+    } catch (err: any) {
+      return super.internalServerError(res, err);
+    }
+  }
+
+  // @route     PUT api/v1/words
+  // @desc      update a word
+  // @access    private
+  public async updateWord(req: Request, res: Response) {
+    try {
+      const wordUpdateDto: WordUpdateDto = req.body;
+      const wordReadDto = await this._wordService.updateWord(wordUpdateDto);
+      if (wordReadDto == null) return super.notFound(res);
+      return super.ok(res, wordReadDto);
+    } catch (err: any) {
+      return super.internalServerError(res, err);
+    }
+  }
+
+  // @route     PUT api/v1/words/user/incrementing-count-of-word-played?wordId=
+  // @desc      increment the count of word played in memory game
+  // @access    private
+  public async incrementCountOfWordPlayed(req: Request, res: Response) {
+    try {
+      const { wordId } = req.params;
+      const wordReadDto = await this._wordService.incrementCountOfWordPlayed(
+        wordId
+      );
+      if (wordReadDto == null) return super.notFound(res);
+      return super.ok(res, wordReadDto);
+    } catch (err: any) {
+      return super.internalServerError(res, err);
+    }
+  }
+
+  // @route     PUT api/v1/words/user/refreshing-count-of-word-played?wordId=
+  // @desc      refresh the count of word played in mory game
+  // @access    private
+  public async refreshCountOfWordPlayed(req: Request, res: Response) {
+    try {
+      const { wordId } = req.params;
+      const wordReadDto = await this._wordService.refreshCountOfWordPlayed(
+        wordId
+      );
+      if (wordReadDto == null) return super.notFound(res);
+      return super.ok(res, wordReadDto);
+    } catch (err: any) {
+      return super.internalServerError(res, err);
+    }
+  }
+
+  // @route     DELETE api/v1/words/user?wordId=
+  // @desc      remove a word
+  // @access    private
+  public async deleteWord(req: Request, res: Response) {
+    try {
+      const { wordId } = req.params;
+      await this._wordService.removeWord(wordId);
+      return super.ok(res);
+    } catch (err: any) {
+      return super.internalServerError(res, err);
+    }
+  }
+}
